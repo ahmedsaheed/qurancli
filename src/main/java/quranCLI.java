43001444483fe5c67acb295d97cbce3ea3ecd430
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-06-26 17:49:07 +0100

    works on terminal correctly, ar text renders LTR

--- src/main/java/quranCLI.java
+++ src/main/java/quranCLI.java
@@ -14,24 +14,29 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
- class getQuran {
-//   javac -cp /Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/info.picocli/picocli/4.6.1/49a67ee4b4d9722fa60f3f9ffaffa72861c32966/picocli-4.6.1.jar;/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/javazoom/jlayer/1.0.1/2bfef7a5a4c9af2184ff74b460b6d7d24349b98a/jlayer-1.0.1.jar;/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okhttp3/okhttp/4.10.0/cd63657ac15770ed1420647154c9f44645533bef/okhttp-4.10.0.jar;/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.json/json/20220320/6df2c050972619466f6dcef7654ef9bcc01dfd0/json-20220320.jar /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/src/main/java/quranCLI.java
-//ja
-     protected static String makeRequest(String url) throws IOException {
-         String responses = "";
-         try {
-             OkHttpClient client = new OkHttpClient();
-             Request request = new Request.Builder()
-                     .url(url)
-                     .build();
-             Response response = client.newCall(request).execute();
-             responses = response.body().string();
-         } catch (Exception e) {
-             System.out.println(e);
-         }
-         return responses;
-     }
-     static void GetFullQuran() {
+
+@Command(name = "quranCLI", mixinStandardHelpOptions = true, version = "quranCLI 1.0",
+        footer = "\nCopyright(c) quranCLI 2022",
+        description = "A simple tool to Read, Search and Recite the Quran\n")
+class quranCLI implements Callable<Integer> {
+    ////   java -cp .:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/info.picocli/picocli/4.6.1/49a67ee4b4d9722fa60f3f9ffaffa72861c32966/picocli-4.6.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/javazoom/jlayer/1.0.1/2bfef7a5a4c9af2184ff74b460b6d7d24349b98a/jlayer-1.0.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okio/okio-jvm/3.0.0/ab5a73fa2ccb4a36b0b5c69fe10b16d0255bcf8/okio-jvm-3.0.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.jetbrains.kotlin/kotlin-stdlib/1.7.0/a5f42c684ad9003160ef0d0f693ecf0ba7b13549/kotlin-stdlib-1.7.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okhttp3/okhttp/4.10.0/cd63657ac15770ed1420647154c9f44645533bef/okhttp-4.10.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.json/json/20220320/6df2c050972619466f6dcef7654ef9bcc01dfd0/json-20220320.jar /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/src/main/java/quranCLI.java
+
+
+    protected static String makeRequest(String url) throws IOException {
+        String responses = "";
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            responses = response.body().string();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return responses;
+    }
+    static void GetFullQuran() {
 
         try {
             OkHttpClient client = new OkHttpClient();
@@ -57,13 +62,13 @@ import java.util.concurrent.Callable;
         JSONArray surahs = data.getJSONArray("ayahs");
 
         for(int i = 0; i < surahs.length(); i++) {
-          JSONObject surah = surahs.getJSONObject(i);
-          JSONObject surah_metadata = surah.getJSONObject("surah");
+            JSONObject surah = surahs.getJSONObject(i);
+            JSONObject surah_metadata = surah.getJSONObject("surah");
             String name = surah_metadata.getString("name");
-          if(!Sura.contains(name)) {
-            Sura.add(name);
-          }
-          juz.append(surah.getString("text")).append("\n");
+            if(!Sura.contains(name)) {
+                Sura.add(name);
+            }
+            juz.append(surah.getString("text")).append("\n");
         }
         juzz.append("Juz number " +Juz+" contains ").append(Sura.size() + 1).append(" Suras\n");
         for (String s : Sura) {
@@ -98,97 +103,83 @@ import java.util.concurrent.Callable;
         return verses.toString();
     }
 
-     public static String getAudio(int surahNumber) throws IOException {
-         String url = "https://api.alquran.cloud/v1/surah/"+surahNumber+"/ar.alafasy";
-         String responses = makeRequest(url);
-         ArrayList<String> Audiourl = new ArrayList<String>();
-         System.out.println(getQuran.GetSurah(surahNumber));
+    public static String getAudio(int surahNumber) throws IOException {
+        String url = "https://api.alquran.cloud/v1/surah/"+surahNumber+"/ar.alafasy";
+        String responses = makeRequest(url);
+        ArrayList<String> Audiourl = new ArrayList<String>();
+        System.out.println(GetSurah(surahNumber));
 
-         try{
-                JSONObject jsonObject = new JSONObject(responses);
-                JSONObject data = jsonObject.getJSONObject("data");
-                JSONArray ayah = data.getJSONArray("ayahs");
-                for(int i = 0; i < ayah.length(); i++) {
-                    JSONObject ayah_data = ayah.getJSONObject(i);
-                    String audio = ayah_data.getString("audio");
-                    Audiourl.add(audio);
-                    playAudio(audio);
+        try{
+            JSONObject jsonObject = new JSONObject(responses);
+            JSONObject data = jsonObject.getJSONObject("data");
+            JSONArray ayah = data.getJSONArray("ayahs");
+            for(int i = 0; i < ayah.length(); i++) {
+                JSONObject ayah_data = ayah.getJSONObject(i);
+                String audio = ayah_data.getString("audio");
+                Audiourl.add(audio);
+                playAudio(audio);
 
-                }
-         }catch (Exception e) {
-             System.out.println(e);
-         }
-
-
-         return responses;
-     }
-
-     public record search (String ArabicName, String EnglishName, String FoundText) {}
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
 
 
-     public static ArrayList search(String keyword) throws IOException {
+        return responses;
+    }
 
-         int sum = 0;
-         ArrayList<search> Matches = new ArrayList<search>();
-         String url = "http://api.alquran.cloud/v1/search/"+keyword+"/all/en";
-         String response = makeRequest(url);
-         JSONObject jsonObject = new JSONObject(response);
-         JSONObject data = jsonObject.getJSONObject("data");
-         JSONArray matched = data.getJSONArray("matches");
-         for(int i = 0; i < matched.length(); i++) {
-             JSONObject surah = matched.getJSONObject(i);
-             JSONObject surah_metadata = surah.getJSONObject("surah");
-             String name = surah_metadata.getString("name");
-             String Englishname = surah_metadata.getString("englishName");
-             String text = surah.getString("text");
-             int wordCount = surah.getInt("numberInSurah");
+    public record search (String ArabicName, String EnglishName, String FoundText) {}
+
+
+    public static ArrayList search(String keyword) throws IOException {
+
+        int sum = 0;
+        ArrayList<search> Matches = new ArrayList<search>();
+        String url = "http://api.alquran.cloud/v1/search/"+keyword+"/all/en";
+        String response = makeRequest(url);
+        JSONObject jsonObject = new JSONObject(response);
+        JSONObject data = jsonObject.getJSONObject("data");
+        JSONArray matched = data.getJSONArray("matches");
+        for(int i = 0; i < matched.length(); i++) {
+            JSONObject surah = matched.getJSONObject(i);
+            JSONObject surah_metadata = surah.getJSONObject("surah");
+            String name = surah_metadata.getString("name");
+            String Englishname = surah_metadata.getString("englishName");
+            String text = surah.getString("text");
+            int wordCount = surah.getInt("numberInSurah");
 //             System.out.println(name);
 //             System.out.println(Englishname);
 //             System.out.println(text);
 //             System.out.println(wordCount);
 //             sum += wordCount;
 //             System.out.println(sum);
-             Matches.add(new search(name, Englishname, text));
+            Matches.add(new search(name, Englishname, text));
 
-         }
+        }
 
-         return Matches;
-     }
+        return Matches;
+    }
 
-     public static void playAudio(String path) {
+    public static void playAudio(String path) {
 
-         try {
-             URL url = new URL(path);
-             BufferedInputStream BIS = new BufferedInputStream(url.openStream());
+        try {
+            URL url = new URL(path);
+            BufferedInputStream BIS = new BufferedInputStream(url.openStream());
 
-             Player player = new Player(BIS);
-             player.play();
+            Player player = new Player(BIS);
+            player.play();
 
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-     }
-
-
-     public static void main(String[] args) throws IOException {
-        getAudio(99);
-         //System.out.println(search("boy"));
-     }
-
-}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
-@Command(name = "quranCLI", mixinStandardHelpOptions = true, version = "quranCLI 1.0",
-        footer = "Copyright(c) quranCLI 2022",
-        description = "A simple tool to Read, Search and Recite the Quran\n")
-class quranCLI implements Callable<Integer> {
-
-    @Option(names = {"-s", "--surah-number"}, description = "find a surah by it's number", interactive = true)
+    @Option(names = {"-s", "--surah-number"}, description = "find a surah by it's number in range 1..114", interactive = true)
     private int surahNumber;
 
-//    @Option(names = {"-a", "--audio"}, description = "plays audio version of surah", interactive = true)
-//    private char audio;
-
+    @Option(names = {"-q", "--query"}, description = "Search a keyword in the quran")
+    private String query;
     @Option(names = {"-a", "--audio"}, description = "plays audio version of surah")
     boolean a;
 
@@ -196,14 +187,17 @@ class quranCLI implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-        if (surahNumber < 1 || surahNumber > 114) {
-            System.out.println("Opps ! The Quran has 114 Surahs. Let's try again");
-            return 0;
-        }
+
         if(a) {
-            getQuran.getAudio(surahNumber);
+            getAudio(surahNumber);
+        }else if(query != null) {
+            System.out.println(search(query).toString());
         }else{
-            System.out.println(getQuran.GetSurah(surahNumber));
+            if (surahNumber < 1 || surahNumber > 114) {
+                System.out.println("Opps ! The Quran has 114 Surahs.\nUse \"quranCLI [command] --help\" for more information about a command.");
+                return 0;
+            }
+            System.out.println(GetSurah(surahNumber));
         }
         return 0;
     }
@@ -224,30 +218,11 @@ class quranCLI implements Callable<Integer> {
     // this example implements Callable, so parsing, error handling and handling user
     // requests for usage help or version help can be done with one line of code.
     public static void main(String... args) {
-        int exitCode = new CommandLine(new quranCLI()).setColorScheme(colorScheme).execute("-h");
+        int exitCode = new CommandLine(new quranCLI()).setColorScheme(colorScheme).execute(args);
 //        System.exit(exitCode);
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-*
-*  java -cp /Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/info.picocli/picocli/4.6.1/49a67ee4b4d9722fa60f3f9ffaffa72861c32966/picocli-4.6.1.jar /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/src/main/java/quranCLI.java /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/hello.txt
- * */
-
-//java -cp /Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/info.picocli/picocli/4.6.1/49a67ee4b4d9722fa60f3f9ffaffa72861c32966/picocli-4.6.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okhttp3/okhttp/4.10.0/cd63657ac15770ed1420647154c9f44645533bef/okhttp-4.10.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/javazoom/jlayer/1.0.1/2bfef7a5a4c9af2184ff74b460b6d7d24349b98a/jlayer-1.0.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.json/json/20220320/6df2c050972619466f6dcef7654ef9bcc01dfd0/json-20220320.jar /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/src/main/java/CheckSum.java
+ java -cp .:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/info.picocli/picocli/4.6.1/49a67ee4b4d9722fa60f3f9ffaffa72861c32966/picocli-4.6.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/javazoom/jlayer/1.0.1/2bfef7a5a4c9af2184ff74b460b6d7d24349b98a/jlayer-1.0.1.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okio/okio-jvm/3.0.0/ab5a73fa2ccb4a36b0b5c69fe10b16d0255bcf8/okio-jvm-3.0.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.jetbrains.kotlin/kotlin-stdlib/1.7.0/a5f42c684ad9003160ef0d0f693ecf0ba7b13549/kotlin-stdlib-1.7.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/com.squareup.okhttp3/okhttp/4.10.0/cd63657ac15770ed1420647154c9f44645533bef/okhttp-4.10.0.jar:/Users/ahmedsaheed/.gradle/caches/modules-2/files-2.1/org.json/json/20220320/6df2c050972619466f6dcef7654ef9bcc01dfd0/json-20220320.jar /Users/ahmedsaheed/Desktop/Desktop/cmdWithJava/src/main/java/quranCLI.java
+ */
