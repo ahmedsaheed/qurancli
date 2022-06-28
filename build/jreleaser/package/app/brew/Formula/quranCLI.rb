6769aad7271e5167402bcb4ec1bbd9061993effe
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-06-29 00:35:42 +0100

    updated build.gradle

--- build/jreleaser/package/app/brew/Formula/quranCLI.rb
+++ build/jreleaser/package/app/brew/Formula/quranCLI.rb
@@ -1,16 +1,12 @@
-# Generated with JReleaser 1.0.0 at 2022-06-28T16:11:44.124288+01:00
+# Generated with JReleaser 1.0.0 at 2022-06-29T00:28:18.954623+01:00
 class App < Formula
   desc "A simple tool to Read, Search and Recite the Quran."
   homepage "https://github.com/ahmedsaheed/quranCLI"
-  url "https://github.com/ahmedsaheed/quranCLI/releases/download/early-access/quranCLI-1.0-SNAPSHOT.zip"
-  version "1.0-SNAPSHOT"
-  sha256 "6de429b313feebfd323055bdb05da3d2806281690d4d8f73ab116ada79630f3a"
+  url "https://github.com/ahmedsaheed/quranCLI/releases/download/v1.0.0/quranCLI-1.0.zip"
+  version "1.0"
+  sha256 "9fca8e95c11aad7f9392025c864b4fa3caace4347aa3ce0c742b492067c0eed9"
   license "Apache-2.0"
 
-  depends_on :key1 => "value1"
-  depends_on "key2" => "value2"
-  depends_on :key3
-  depends_on "key4"
   depends_on "openjdk@17"
 
   def install
@@ -20,6 +16,6 @@ class App < Formula
 
   test do
     output = shell_output("#{bin}/quranCLI --version")
-    assert_match "1.0-SNAPSHOT", output
+    assert_match "1.0", output
   end
 end
